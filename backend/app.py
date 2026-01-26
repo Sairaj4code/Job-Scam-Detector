0493f4e3eef7b0d2fbf11ff99babe66d302e6ed9
Sairaj4code <sairajkhot57@gmail.com>
2026-01-26 21:17:13 +0530

    Backend

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from routes.analyze import analyze_bp
+from backend.routes.analyze import analyze_bp
 from flask_cors import CORS 
 from routes.report import report_bp
 from routes.chat import chat_bp
